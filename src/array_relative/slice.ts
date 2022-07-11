b8d347067233a12cfb2e0e0f0ace28f2ef6bf677
sunyaling <sunyaling@tianyancha.com>
2022-07-11 14:31:11 +0800

    feat(array-relative): slice

--- src/array_relative/slice.ts
+++ src/array_relative/slice.ts
@@ -5,10 +5,29 @@
  * @param end 可选参数，提取终止处的索引，提取不包含该位置元素，省略则默认到原数组的末尾
  * @returns Array 提取元素的新数组
  */
-// Array.prototype.slice = function (start?: number, end?: number): T[] {
 
-// }
+Array.prototype.slice = function <T>(start = 0, end?: number): T[] {
+  const array = Object(this);
+  const _len = array.length;
+  let result: T[] = [];
+
+  let formIndex = start >= 0 ? start : Math.max(0, _len + start);
+  let toIndex = typeof end === 'number' ? Math.min(end, _len) : _len;
+  toIndex = toIndex < 0 ? _len + end : end;
+
+  const size = toIndex - formIndex;
+
+  if (size > 0) {
+    for (let i = formIndex; i < toIndex; i++) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
 const fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
 const citrus = fruits.slice(1, 3);
-console.log(citrus) // ['Orange', 'Lemon']
-console.log(fruits) // ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+const res = fruits.slice(-2, -1);
+console.log(citrus); // ['Orange', 'Lemon']
+console.log(res); // [ 'Apple' ]
+console.log(fruits); // ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
